c6d1feefe34a9e8623891aabc2767bd3c99d90da
DimaS <dima@sukhikh.xyz>
2025-12-05 19:35:25 +0100

    Solve day-02 part1

---
+++ 2025/rust/day-02/src/part1.rs
@@ -0,0 +1,49 @@
+use std::ops::RangeInclusive;
+use miette::miette;
+
+use nom::character::complete::line_ending;
+use nom::combinator::{all_consuming, consumed};
+use nom::{IResult, Parser};
+use nom::{character::complete, multi::separated_list1, sequence::separated_pair, bytes::complete::tag};
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+    let (_, id_ranges) =
+        ranges.parse(input).map_err(|e| miette!("failed to parse {e}"),)?;
+    let mut total = 0;
+
+    for ids in id_ranges.into_iter() {
+        for id in ids.into_iter() {
+            let id_str = id.to_string();
+            let half = id_str.len() / 2;
+            if id_str[..half] == id_str[half..] {
+               total += id;
+            }
+        }
+    }
+    
+    Ok(total.to_string())
+}
+
+fn ranges(input: &str) -> IResult<&str, Vec<RangeInclusive<u64>>> {
+    separated_list1(
+        tag(","),
+        separated_pair(
+            complete::u64,
+            tag("-"),
+            complete::u64
+        ).map(|(start, end)| start..=end),).parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+";
+        assert_eq!("1227775554", process(input)?);
+        Ok(())
+    }
+}
